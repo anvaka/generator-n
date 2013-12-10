0aef68dbce5041f64ffc8b8866f074b792576290
anvaka <anvaka@gmail.com>
2013-12-10 02:48:20 -0800

    Added creation of main package file

--- app/buildFilesFromPackageJson.js
+++ app/buildFilesFromPackageJson.js
@@ -1,4 +1,5 @@
 /* This file constructs project tree based on information provided in package.json */
+var fs = require('fs');
 
 module.exports = function () {
   var packageJson = require('./readPackageJson')();
@@ -6,10 +7,28 @@ module.exports = function () {
     return; // How could this happen?
   }
 
+  initPackageVariables.bind(this)(packageJson);
+  copyTemplates.bind(this)(packageJson);
+  createEntryPoint.bind(this)(packageJson.main);
+};
+
+function copyTemplates(packageJson) {
+  this.copy('.gitignore', '.gitignore');
+  this.template('_readme.md', 'README.md');
+}
+
+function createEntryPoint(mainFileName) {
+  if (!mainFileName) {
+    this.log.error('Main package file name is missing. File will not be created');
+    return;
+  }
+  // create file if it does not exist:
+  var fd = fs.openSync(mainFileName, 'a');
+  fs.closeSync(fd);
+}
+
+function initPackageVariables(packageJson) {
   this.packageName = packageJson.name;
   this.packageLicense = packageJson.license;
   this.packageDescription = packageJson.description;
-
-  this.copy('.gitignore', '.gitignore');
-  this.template('_readme.md', 'README.md');
-};
+}
