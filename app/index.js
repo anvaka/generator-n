324270b46efbcbde9b78a775c2032d8c97f485c4
anvaka <anvaka@gmail.com>
2013-12-10 02:11:43 -0800

    Extracted npmInit into own file

--- app/index.js
+++ app/index.js
@@ -16,27 +16,7 @@ var NGenerator = module.exports = function NGenerator(args, options, config) {
 
 util.inherits(NGenerator, yeoman.generators.Base);
 
-NGenerator.prototype.npmInit = function () {
-  // let's see if we have package.json already:
-  if (require('./readPackageJson')()) {
-    return;
-  }
-
-  // no package.json. Proceed to creation
-  var cb = this.async();
-  console.log(chalk.yellow.bold('npm init'));
-  this.spawnCommand('npm', ['init'], cb)
-    .on('error', cb)
-    .on('exit', function (err) {
-      if (err === 1) {
-        this.log.error('Cancelled npm init. Terminating generator');
-      } else if (err === 127) {
-        this.log.error('Could not find ' + installer + '. Please install with ' +
-                            '`npm install -g ' + installer + '`.');
-      }
-      cb(err);
-    }.bind(this));
-};
+NGenerator.prototype.npmInit = require('./npmInit.js');
 
 NGenerator.prototype.buildFilesFromPackage = require('./buildFilesFromPackageJson');
 
---
+++ app/npmInit.js
@@ -0,0 +1,23 @@
+var chalk = require('chalk');
+
+module.exports = function () {
+  // let's see if we have package.json already:
+  if (require('./readPackageJson')()) {
+    return;
+  }
+
+  // no package.json. Proceed to creation
+  var done = this.async();
+  console.log(chalk.yellow.bold('npm init'));
+  this.spawnCommand('npm', ['init'], done)
+    .on('error', done)
+    .on('exit', function (err) {
+      if (err === 1) {
+        this.log.error('Cancelled npm init. Terminating generator');
+      } else if (err === 127) {
+        this.log.error('Could not find ' + installer + '. Please install with ' +
+                            '`npm install -g ' + installer + '`.');
+      }
+      done(err);
+    }.bind(this));
+};
