3038fb97bc8a449a815c09034d96d026dcd829b1
anvaka <anvaka@gmail.com>
2013-12-10 02:05:27 -0800

    Added readme.md file generation

--- app/buildFilesFromPackageJson.js
+++ app/buildFilesFromPackageJson.js
@@ -1,5 +1,15 @@
 /* This file constructs project tree based on information provided in package.json */
 
 module.exports = function () {
+  var packageJson = require('./readPackageJson')();
+  if (!packageJson) {
+    return; // How could this happen?
+  }
 
+  this.packageName = packageJson.name;
+  this.packageLicense = packageJson.license;
+  this.packageDescription = packageJson.description;
+
+  this.copy('.gitignore', '.gitignore');
+  this.template('_readme.md', 'README.md');
 };
---
+++ app/templates/_readme.md
@@ -0,0 +1,15 @@
+# <%= packageName %>
+
+<%= packageDescription %>
+
+# install
+
+With [npm](https://npmjs.org) do:
+
+```
+npm install <%= packageName %>
+```
+
+# license
+
+<%= packageLicense %>
